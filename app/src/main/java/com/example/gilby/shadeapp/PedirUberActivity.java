f9b81dceeee5ee493d6b3e2f86d98467194e1f22
Rene Arzeno <re_frans@hotmail.com>
2018-09-27 23:17:45 -0400

    mapa con ubicacion actual
    esto tambien sale?? ::
    ==
    
    <resources>
        <!--
        TODO: Before you run your application, you need a Google Maps API key.

--- app/src/main/java/com/example/gilby/shadeapp/PedirUberActivity.java
+++ app/src/main/java/com/example/gilby/shadeapp/PedirUberActivity.java
@@ -5,8 +5,12 @@ import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class PedirUberActivity extends AppCompatActivity {
+
+    private LinearLayout Posicionhome;
 
     private GestureDetectorCompat gestureDetectorCompat = null;
 
@@ -14,6 +18,7 @@ public class PedirUberActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pedir_uber);
+        Posicionhome = (LinearLayout)findViewById(R.id.lugar_casa)  ;
 
         // Create a common gesture listener object.
         DetectSwipeGestureListener gestureListener = new DetectSwipeGestureListener();
@@ -23,6 +28,17 @@ public class PedirUberActivity extends AppCompatActivity {
 
         // Create the gesture detector with the gesture listener.
         gestureDetectorCompat = new GestureDetectorCompat(this, gestureListener);
+
+        Posicionhome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent regist = new Intent(PedirUberActivity.this, MapaUberActivity.class);
+                startActivity(regist);
+                finish();
+            }
+
+        });
+
 
     }
 
